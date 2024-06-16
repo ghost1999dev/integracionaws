0ea237abf5a96d57694ae3a85778a1afe08abc53
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2024-06-16 17:13:25 -0600

    Ultimo Commit

--- upload.bat
+++ upload.bat
@@ -1,8 +1,17 @@
+# Paso 1: Agregar y cometer cambios en Git
 git add .
 git commit -m "Ultimo Commit"
-git push 
-set GOARCH=amd64 
-set GOOS=linux 
-go build main.go
-del deployment.zip bootstrap
+git push
+
+# Paso 2: Configurar variables de entorno para la compilación cruzada de Go
+set GOARCH=amd64
+set GOOS=linux
+
+# Paso 3: Construir el binario de Go
+go build -o bootstrap main.go
+
+# Paso 4: Eliminar el archivo ZIP previo si existe
+del deployment.zip
+
+# Paso 5: Crear un nuevo archivo ZIP con el binario
 tar.exe -a -cf deployment.zip bootstrap
