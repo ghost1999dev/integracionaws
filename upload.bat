9c37bd10669885cac0805e188c3ae8d8eb310564
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2024-06-16 17:06:44 -0600

    Ultimo Commit

--- upload.bat
+++ upload.bat
@@ -1,8 +1,6 @@
 git add .
 git commit -m "Ultimo Commit"
 git push 
-set GOOS=linux
-set GOARCH=amd64
-go build main.go
-del main.zip
-tar.exe -a -cf main.zip main
+set GOARCH=amd64 
+set GOOS=linux go build -o bootstrap main.go
+zip deployment.zip bootstrap
